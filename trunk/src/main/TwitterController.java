e72930561a219bcd294badae1c80f747df52185f
zealotzuo <zealotzuo@426a098e-5d83-11de-8abc-d1c337b90d21>
2009-07-28 21:40:38 +0000

    Added GNU General Public License v3

--- trunk/src/main/TwitterController.java
+++ trunk/src/main/TwitterController.java
@@ -1,3 +1,23 @@
+/*
+	COMMAND LINE INTERFACE TWITTER CLIENT
+	COPYRIGHT (C) ZEE ZUO 2009
+
+	This file is part of CliTweet.
+
+	CliTweet is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	CliTweet is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License 
+	along with CliTweet.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package main;
 
 import java.util.Iterator;
